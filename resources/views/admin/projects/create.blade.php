8fc7e57485f01f6a02604eacc3f426bcf5ba9409
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-07-25 15:11:21 +0200

    [UPD] Add checkbox in create

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <section>
         <div class="container">
-            <h1 class="text-white">Aggiungi un nuovo Progetto</h1>
+            <h1 class="pt-5 text-white">Aggiungi un nuovo Progetto</h1>
 
             @if ($errors->any())
                 <div class="mt-5 alert alert-danger">
@@ -18,7 +18,7 @@
                     </ul>
                 </div>
             @endif
-            <form class="mt-5" action="{{ route('projects.store') }}" method="POST">
+            <form class="mt-5" action="{{ route('admin.projects.store') }}" method="POST">
                 @csrf
                 {{-- TITLE --}}
                 <div class="text-white form-group">
@@ -38,41 +38,43 @@
                 </div>
 
                 {{-- TECHNOLOGY --}}
-                <div class="mt-3 text-white form-group">
-                    <label for="technologies">Technologies</label>
+                <div class="mt-5 text-white form-group">
+                    <label for="technologies">Technologies : </label>
 
-                    {{-- @foreach ($technologies as $technology)
-                        <input type="checkbox" name="technologies[]" class="btn-check" id="{{}}"
-                            autocomplete="off">
-                        <label class="btn" for="btn-check-4">{{ $technology->name }}</label>
+                    @foreach ($technologies as $technology)
+                        <div class="form-check form-check-inline">
+                            <input type="checkbox" name="technologies" class="text-white btn-check"
+                                id="tech-{{ $technology->id }}" autocomplete="off">
+                            <label class="btn text-white" for="tech-{{ $technology->id }}">{{ $technology->name }}</label>
+                        </div>
                     @endforeach
 
-                    <input type="checkbox" class="btn-check" id="btn-check-5" checked autocomplete="off">
+                    {{-- <input type="checkbox" class="btn-check" id="btn-check-5" checked autocomplete="off">
                     <label class="btn" for="btn-check-5">Checked</label>
 
                     <input type="checkbox" class="btn-check" id="btn-check-6" autocomplete="off" disabled>
                     <label class="btn" for="btn-check-6">Disabled</label> --}}
 
-                    <select name="technologies[]" id="technologies" class="form-control" multiple>
+                    {{-- <select name="technologies[]" id="technologies" class="form-control" multiple>
                         @foreach ($technologies as $technology)
                             <option value="{{ $technology->id }}">{{ $technology->name }}</option>
                         @endforeach
-                    </select>
+                    </select> --}}
                 </div>
 
                 {{-- DESCRIPTION --}}
-                <div class="text-white mt-3 form-group">
+                <div class="text-white mt-4 form-group">
                     <label for="description">Descrizione</label>
                     <textarea name="description" id="description" class="form-control" required>Progetto simile a Netflix</textarea>
                 </div>
 
                 {{-- STATUS --}}
-                <div class="text-white mt-3 form-group">
+                <div class="text-white mt-4 form-group">
                     <label for="status">Status</label>
                     <input value="WIP" type="text" name="status" id="status" class="form-control" required>
                 </div>
 
-                {{-- <div class="text-white mt-3 form-group">
+                {{-- <div class="text-white mt-4 form-group">
                     <label for="status">Status</label>
                     <select name="status" id="status" class="form-control" required>
                         <option value="Done" {{ $project->status == 'Done' ? 'selected' : '' }}>Done</option>
@@ -82,25 +84,25 @@
                 </div> --}}
 
                 {{-- START DATE --}}
-                <div class="text-white mt-3 form-group">
+                <div class="text-white mt-4 form-group">
                     <label for="start_date">Data Inizio</label>
                     <input value="2020-05-07" type="date" name="start_date" id="start_date" class="form-control">
                 </div>
 
                 {{-- END DATE --}}
-                <div class="text-white mt-3 form-group">
+                <div class="text-white mt-4 form-group">
                     <label for="end_date">Data Fine</label>
                     <input type="date" name="end_date" id="end_date" class="form-control">
                 </div>
 
                 {{-- IMAGES --}}
-                <div class="text-white mt-3 form-group">
+                <div class="text-white mt-4 form-group">
                     <label for="images">Immagini (separate da virgola)</label>
                     <input
                         value="https://yt3.googleusercontent.com/i0PPodnHGyWQ9m2XSQ2pjzxhKfSwoK2eI2DlcFjFdsmLqE3jMhBkkh-Hihato9qMcZf9t1ekFQ=s900-c-k-c0x00ffffff-no-rj"
                         type="text" name="images" id="images" class="form-control" required>
                 </div>
-                <button type="submit" class="btn btn-primary mt-3">Create Project</button>
+                <button type="submit" class="btn btn-primary mt-4">Create Project</button>
             </form>
         </div>
     </section>
